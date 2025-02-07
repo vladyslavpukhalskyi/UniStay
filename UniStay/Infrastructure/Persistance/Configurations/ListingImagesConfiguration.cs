4b865421125d48384700bc115da050e546967b72
lonellylennon <vladislavackerman@gmail.com>
2025-02-07 17:48:49 +0200

    Configuration

--- UniStay/Infrastructure/Persistance/Configurations/ListingImagesConfiguration.cs
+++ UniStay/Infrastructure/Persistance/Configurations/ListingImagesConfiguration.cs
@@ -1,6 +1,38 @@
-namespace Infrastructure.Persistance.Configurations;
+using Domain.ListingImages;
+using Domain.Listings;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class ListingImagesConfiguration
+namespace Infrastructure.Persistance.Configurations
 {
-    
+    public class ListingImagesConfiguration : IEntityTypeConfiguration<ListingImage>
+    {
+        public void Configure(EntityTypeBuilder<ListingImage> builder)
+        {
+            builder.ToTable("ListingImages");
+            
+            builder.HasKey(li => li.Id);
+            
+            builder.Property(li => li.Id)
+                .HasConversion(
+                    id => id.Value, 
+                    value => new ListingImageId(value))
+                .ValueGeneratedNever();
+            
+            builder.Property(li => li.ListingId)
+                .HasConversion(
+                    id => id.Value, 
+                    value => new ListingId(value))
+                .IsRequired();
+            
+            builder.Property(li => li.ImageUrl)
+                .IsRequired()
+                .HasMaxLength(2048);
+            
+            builder.HasOne(li => li.Listing)
+                .WithMany(l => l.ListingImages)
+                .HasForeignKey(li => li.ListingId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
 }
